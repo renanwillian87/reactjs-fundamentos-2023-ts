be55143c178f1a870d110a62c5f3d920d48f594d
Renan Santos <renanwillian87@gmail.com>
2023-07-06 16:19:48 -0300

    chore: init project ts with code js

---
+++ src/components/Comment.tsx
@@ -0,0 +1,53 @@
+import { Avatar } from './Avatar';
+import { Trash, ThumbsUp } from 'phosphor-react';
+import styles from './Comment.module.css';
+import { useState } from 'react';
+
+interface CommentProps {
+    content: string;
+    onDeleteComment: (commentToDelete: string) => void;
+}
+
+export function Comment({ content, onDeleteComment }: CommentProps) {
+    const [likeCount, setLikeCount] = useState(0)
+
+    function handleDeleteComment() {
+        onDeleteComment(content)
+    }
+
+    function handleLikeComment() {
+        setLikeCount((state) => {
+            return state + 1;
+        });
+    }
+
+    return(
+        <div className={styles.comment}>
+            <Avatar hasBorder={false} src="https://github.com/renanwillian87.png" alt=""/>
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Renan Santos</strong>
+                            <time title="04 de Julho às 21:58" dateTime="2023-07-04 21:58">Cerca de 1h atrás</time>
+                        </div>
+
+                        <button onClick={handleDeleteComment} title='Deletar comentário'>
+                            <Trash size={24}/>
+                        </button>
+                    </header>
+
+                    <p>{content}</p>
+                </div>
+
+                <footer>
+                    <button onClick={handleLikeComment}>
+                        <ThumbsUp/>
+                        Aplaudir <span>{likeCount}</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
